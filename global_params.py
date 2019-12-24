abcac025b900503fe39c5835f7920f3e61f2066c
Udayshankar <pyro@UdayshankarsMBP.wavecable.com>
2019-12-23 17:19:21 -0800

    update

--- global_params.py
+++ global_params.py
@@ -1,5 +1,15 @@
+#! /usr/bin/env python3
+
+print("\n\t\t\tWelcome to Bardvja's ashram ...\n")
+# Change your search queries as required. 
+# These topics are scraped from ArXiv using the ArXiv API.
+# The module arXiv_api does this and you can make changes to your requirement there if necessary.
 search_queries = ['physics','fluid','particle']
+
+# Change paths to each folders as necessary. 
 path_to_papers_train = './../papers_train/'
 path_to_training_data = './../training_data/'
 path_to_trained_models = './../trained_models/'
 path_to_arxiv_data = './../arxiv_data/'
+
+print(f"Please take your shoes off and watch the turtles on the floor!\n")
---
+++ main.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+import global_params
+#import arXiv_api
+import pdf_reader
+import arxiv_data_vectors
+import recommender
+
